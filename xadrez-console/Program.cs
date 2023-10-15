94ce1af0bde687484dd91da6a28260e8ecc68da2
Leandro Oliveira da Silva <lleandro.silva@outlook.com>
2023-10-15 16:15:33 -0300

    commit inicial

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+             try
+             {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                 while (!partida.terminada) {
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                        partida.validarPosicaoDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+               
+                 Console.Clear();
+                Tela.imprimirPartida(partida);
+
+            }
+             catch (Exception ex)
+             {
+                 Console.WriteLine(ex.Message);
+             }
+           
+    
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
